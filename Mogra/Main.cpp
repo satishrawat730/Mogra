b4b9e2a6ff0938e6d06b8ac52cff904291c953cc
Satish Rawat <satishrawat730@gmail.com>
2023-01-09 22:42:13 +0530

    + Win Main * system from console to window

---
+++ Mogra/Main.cpp
@@ -0,0 +1,13 @@
+/// Mogra - Modern Graphics Application
+
+// Windows Header Files:
+#include <windows.h>
+
+#define WINDOW_TITLE    "Mogra Editor"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpcmdLine, int cmdShow)
+{
+   
+
+    return 0;
+}
